cc70e2b417569abeb2f7ecc2db2529f7d8d03b49
SriRam S <sri223770@gmail.com>
2025-10-08 11:29:25 +0530

    Interlaced odd/even

--- interlaced_odd_even.c
+++ interlaced_odd_even.c
@@ -1,20 +1,81 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int main()
+void main()
 {
-
-int x,y,i,j;
-scanf("%d%d",&x,&y);
-for(i=x,j=y;i<=y;i++,j--)
-{
-    if(i%2==1)
-    {
-        printf("%d ",i);
-    }
-    if(j%2==0)
-    {
-        printf("%d ",j);
-    }
-}
+	int a,b,v1,v2,i,j,k;
+	scanf("%d %d",&a,&b);
+	i=a;
+	v1=a;
+	v2=b;
+	while(i>=a && i<=b)
+	{
+		if(a%2==0)
+		{
+			j=v2;
+			k=v1;
+			for(;j>=a;)
+			{
+				if(j%2==0)
+				{
+				printf("%d ",j);
+			    v2=j-1;
+			    break;
+				}
+				else
+				{
+					v2=j-1;
+					break;
+				}
+			}
+			for(;k<=b;)
+			{
+				if(k%2==1)
+				{
+					printf("%d ",k);
+					v1=k+1;
+					break;
+				}
+				else
+				{
+					v1=k+1;
+					break;
+				}
+			}
+		}
+		
+		if(a%2==1||(a%2==0&&b%2==1))
+		{
+			k=v1;
+		    j=v2;
+			for(;k<=b;)
+			{
+				if(k%2==1)
+				{
+					printf("%d ",k);
+					v1=k+1;
+					break;
+				}
+				else
+				{
+					v1=k+1;
+					break;
+				}
+			
+			}
+			for(;j>=a;)
+			{
+				if(j%2==0)
+				{
+					printf("%d ",j);
+					v2=j-1;
+					break;
+				}
+				else
+				{
+					v2=j-1;
+					break;
+				}
+			}
+		}
+		i++;
+	}
 }
